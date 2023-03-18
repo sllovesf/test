7531405faec35145515fb0ade8c848b3cc18b2ec
jiangsonglin <2196434256@qq.com>
2023-03-18 14:22:57 +0800

    c语言学习

---
+++ test3_18/test.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+//int main() 
+////{
+////	double a = 5 / 2.0;
+////	printf("a = %lf\n",a);
+////	return 0;
+////}
+//int main()
+//{
+//	int a = -1;
+//	int b = a >> 1;
+//	printf("%d",b  );
+//	return 0;
+//}
+//int main()
+//{
+//	int a = -5;
+//	int b = a << 1;
+//	//00000000000000000000000000000101
+//	printf("%d\n",b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a ^ b;
+//	//011
+//	//101 
+//	//001
+//	printf("%d", c);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	/*临时变量int tmp = a;
+//	a = b;
+//	b = tmp;
+//	printf("%d%d%d",a,b,tmp );*/
+//	//加减法——可能会溢出。
+//	// a = a + b;//8
+//	//b = a - b;//5
+//	//a = a - b;
+//	//printf("%d %d", a, b);
+//	异或法：
+// b = a ^ b;
+//	a = a ^ b;
+//	b = a ^ b;
+//	printf("%d %d", a, b);
+//int main()
+//{
+//	int num = 0;
+//	int count = 0;
+//	scanf("%d", &num);
+//	/*while (num)
+//	{
+//		if (num % 2 == 1)
+//			count++;
+//		num = num / 2;
+//	}*/
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (1 == ((num = num >> 1) & 1))
+//			count++;
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+int main()
+{
+	int a = 0;
+	if (a== 1)
+		printf("hehe");
+	return 0;
+}
