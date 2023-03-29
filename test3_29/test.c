5123bb6169b8424cfda3fb5c873a8eb93b3bd21e
jiangsonglin <2196434256@qq.com>
2023-03-29 20:04:19 +0800

    1

---
+++ test3_29/test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a = 0x11223344;
+	int* pa = &a;
+	char* pc = &a;
+	printf("%p\n", pa);
+	printf("%p\n", pa + 1);
+	printf("%p\n", pc);
+	printf("%p\n", pc + 1);
+	return 0;
+}
