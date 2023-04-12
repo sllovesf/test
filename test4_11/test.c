f9384822ec3237ebb55a32015487d2dda13ed39d
jiangsonglin <2196434256@qq.com>
2023-04-12 11:23:18 +0800

    1

--- test4_11/test.c
+++ test4_11/test.c
@@ -152,102 +152,109 @@
 //
 //	return 0；
 //}
-struct stu 
-{
-	char name[20];
-	int age;
-};
-int a(const void* e1, const void* e2)
-{
-	return ((int)(*(float*)e1 - *(float*)e2));
-}
-void test()
-{
-	float f[] = { 9.0,7.0,6.5,4.5 };
-	int sz = sizeof(f) / sizeof(f[0]);
-	qsort(f, sz, sizeof(f[0]), a);
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("%f ",f[i]);
-	}
-}
-int b_age(const void* e1,const void*e2)
-{
-	return((struct stu*)e1)->age - ((struct stu*)e2)->age;
-}int b_name(const void* e1,const void*e2)
-{
-	return strcmp(((struct stu*)e1)->name , ((struct stu*)e2)->name);
-}
-void test1()
-{
-	struct stu s[3] = { {"zhangsan",20},{"lisi",30},{"wangwu",10} };
-	int sz = sizeof(s) / sizeof(s[0]);
-	qsort(s, sz, sizeof(s[0]), b_age);
-	qsort(s, sz, sizeof(s[0]), b_name);
-	//1.首元素地址
-	//2.元素个数
-	//3.每个元素大小
-	//4.函数指针：比较两个元素的所用函数的地址-这个函数使用者自己实
-	//            现函数指针的两个叁数是：待比较的两个元素的地址
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		//printf("%s ", s->name);
-		printf("%d ", s->age);
-	}
-}
-void swap(char* buf1, char* buf2, int width)
-{
-	int i = 0;
-	for (i = 0; i < width; i++)
-	{
-		char tmp = *buf1;
-		*buf1 = *buf2;
-		*buf2 = tmp;
-		buf1++;
-		buf2++;
-	}
-}
-int cmp_int(const void* e1, const void* e2)
-{
-	return *(int*)e1 - *(int*)e2;
-}
-//实现冒泡排序的程序员，他是否知道未来排序的数据类型-不知道
-//那程序员也不知道，待比较的两个元素的类型
-void sort(void* base,int sz,int width,int(*cmp)(void* e1,void* e2))
-{
-	int i = 0;
-	//趟数
-	for (i = 0; i < sz - 1; i++)
-	{
-		//每一趟比较的对数
-		int j = 0;
-		for (j = 0; j < sz - 1; j++)
-		{
-			//两个元素的比较
-			if (cmp((char*)base+j*width,(char*)base+(j+1)*width) > 0)
-			{
-				//交换
-				swap((char*)base + j * width, (char*)base + (j + 1) * width,width);
-			}
-		}
-	}
-}
-void test4()
-{
-	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	sort(arr,sz,sizeof(arr[0]),cmp_int);
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-
-}
+//struct stu 
+//{
+//	char name[20];
+//	int age;
+//};
+//int a(const void* e1, const void* e2)
+//{
+//	return ((int)(*(float*)e1 - *(float*)e2));
+//}
+//void test()
+//{
+//	float f[] = { 9.0,7.0,6.5,4.5 };
+//	int sz = sizeof(f) / sizeof(f[0]);
+//	qsort(f, sz, sizeof(f[0]), a);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%f ",f[i]);
+//	}
+//}
+//int b_age(const void* e1,const void*e2)
+//{
+//	return((struct stu*)e1)->age - ((struct stu*)e2)->age;
+//}int b_name(const void* e1,const void*e2)
+//{
+//	return strcmp(((struct stu*)e1)->name , ((struct stu*)e2)->name);
+//}
+//void test1()
+//{
+//	struct stu s[3] = { {"zhangsan",20},{"lisi",30},{"wangwu",10} };
+//	int sz = sizeof(s) / sizeof(s[0]);
+//	qsort(s, sz, sizeof(s[0]), b_age);
+//	qsort(s, sz, sizeof(s[0]), b_name);
+//	//1.首元素地址
+//	//2.元素个数
+//	//3.每个元素大小
+//	//4.函数指针：比较两个元素的所用函数的地址-这个函数使用者自己实
+//	//            现函数指针的两个叁数是：待比较的两个元素的地址
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		//printf("%s ", s->name);
+//		printf("%d ", s->age);
+//	}
+//}
+//void swap(char* buf1, char* buf2, int width)
+//{
+//	int i = 0;
+//	for (i = 0; i < width; i++)
+//	{
+//		char tmp = *buf1;
+//		*buf1 = *buf2;
+//		*buf2 = tmp;
+//		buf1++;
+//		buf2++;
+//	}
+//}
+//int cmp_int(const void* e1, const void* e2)
+//{
+//	return *(int*)e1 - *(int*)e2;
+//}
+////实现冒泡排序的程序员，他是否知道未来排序的数据类型-不知道
+////那程序员也不知道，待比较的两个元素的类型
+//void sort(void* base,int sz,int width,int(*cmp)(void* e1,void* e2))
+//{
+//	int i = 0;
+//	//趟数
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		//每一趟比较的对数
+//		int j = 0;
+//		for (j = 0; j < sz - 1; j++)
+//		{
+//			//两个元素的比较
+//			if (cmp((char*)base+j*width,(char*)base+(j+1)*width) > 0)
+//			{
+//				//交换
+//				swap((char*)base + j * width, (char*)base + (j + 1) * width,width);
+//			}
+//		}
+//	}
+//}
+//void test4()
+//{
+//	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	sort(arr,sz,sizeof(arr[0]),cmp_int);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//
+//}
+//int main()
+//{
+//	test4();
+//	return 0;
+//}
 int main()
 {
-	test4();
+	int a[] = {1,2,3,4};
+	printf("%d", sizeof(a + 0));
+	printf("%d", sizeof(*a));
 	return 0;
 }
